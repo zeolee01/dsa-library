1825302766b50abea74c4252f61f0bb2ee2b5f27
akhil <sharma.akhil484@gmail.com>
2022-10-01 17:59:26 +0530

    Added queue implementation in python

---
+++ Data Structures/Queue/queue.py
@@ -0,0 +1,44 @@
+# Contributed by - Akhil Kumar Sharma <sharma.akhil484@gmail.com>
+
+class queue:
+	def __init__(self):
+		self.front=-1
+		self.rear=-1
+		self.ls=[]
+
+	def Enqueue(self,data):
+		if self.front==-1:
+			self.front=self.rear=0
+			self.ls.append(data)
+		else:
+			self.rear=self.rear+1
+			self.ls.append(data)
+
+	def Dequeue(self):
+		if(self.front==self.rear):
+			self.front=self.rear=-1
+		else:
+			d=self.ls.pop(self.front)
+			self.rear=self.rear-1
+			
+
+	def display(self):
+		if self.front==-1:
+			print("Queue is Empty")
+		else:
+			print("Queue:")
+			for i in range(0,self.rear+1):
+				
+				print(self.ls[i])
+
+
+q_obj=queue()
+q_obj.__init__()
+q_obj.Enqueue(4)
+q_obj.Enqueue(6)
+q_obj.Enqueue(1)
+q_obj.Enqueue(7)
+q_obj.Enqueue(2)
+q_obj.display()
+q_obj.Dequeue()
+q_obj.display()
