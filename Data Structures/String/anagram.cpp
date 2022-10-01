e2ff15299e1c8b76c5bd7996151597ecd0535ccb
HARSH PRATAP SINGH <hpsingh9536@gmail.com>
2022-10-02 00:26:00 +0530

    Update anagram.cpp

--- Data Structures/String/anagram.cpp
+++ Data Structures/String/anagram.cpp
@@ -1,9 +1,9 @@
-# Contributed by - Harsh Pratap Singh <hpsingh9536@gmail.com>
+/* Contributed by - Harsh Pratap Singh <hpsingh9536@gmail.com>
 
-// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
+* An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+*/
 
 #include<bits/stdc++.h>
 using namespace std ;
